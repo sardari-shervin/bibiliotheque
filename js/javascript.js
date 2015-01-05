654d81dcf2e0e53f7db977267c91702559cfa0b3
Shervin Sardari <shervin2502@gmail.com>
2015-01-05 09:20:13 +0100

    menus
    voici une bibliothèque d’animations de menu.

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    $('.menulien').on('click', function(){
+  		$('.menu').toggleClass('clickMenu');
+	});
+	$('.menuliendeux').on('click', function(){
+  		$('.menudeux').toggleClass('clickMenudeux');
+	});
+	$('.menulientrois').on('click', function(){
+  		$('.menutrois').toggleClass('clickMenutrois');
+	});
+	$('.menulienquatre').on('click', function(){
+  		$('.menuquatre').toggleClass('clickMenuquatre');
+	});
+	$('.menuliencinq').on('click', function(){
+  		$('.menucinq').toggleClass('clickMenucinq');
+	});
+	$('.menuliencinq').on('click', function(){
+  		$('.menuliencinq').toggleClass('clickMenucinqbis');
+	});
+});
+
+
